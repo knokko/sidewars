27b5f3779f6c2733701ee095d704f178c3dab749
knokko <knokogator@hotmail.com>
2020-01-04 16:58:14 +0100

    Some refactoring and some almost empty new classes

---
+++ sidewars-core/src/main/java/org/grandknock/sidewars/core/arena/Arena.java
@@ -0,0 +1,10 @@
+package org.grandknock.sidewars.core.arena;
+
+public class Arena {
+
+    public final ArenaPrototype prototype;
+
+    Arena(ArenaPrototype prototype){
+        this.prototype = prototype;
+    }
+}
--- sidewars-core/src/main/java/org/grandknock/sidewars/core/arena/ArenaPrototype.java
+++ sidewars-core/src/main/java/org/grandknock/sidewars/core/arena/ArenaPrototype.java
@@ -1,4 +1,6 @@
 package org.grandknock.sidewars.core.arena;
 
 public class ArenaPrototype {
+
+    private String name;
 }
