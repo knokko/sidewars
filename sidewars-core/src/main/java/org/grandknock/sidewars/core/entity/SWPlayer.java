c6125b8cceab0ca884170b8644bf6941a9bd3db2
GrantisJ <grantasjo@gmail.com>
2020-01-12 15:49:42 +0200

    SWPlayer implemented SWCommandSender

--- sidewars-core/src/main/java/org/grandknock/sidewars/core/entity/SWPlayer.java
+++ sidewars-core/src/main/java/org/grandknock/sidewars/core/entity/SWPlayer.java
@@ -1,4 +1,16 @@
 package org.grandknock.sidewars.core.entity;
 
-public class SWPlayer {
+import org.grandknock.sidewars.core.command.SWCommandSender;
+
+public class SWPlayer implements SWCommandSender {
+
+    @Override
+    public boolean hasPermission(String permission) {
+        
+    }
+
+    @Override
+    public void sendMessage(MessageType type, String message) {
+
+    }
 }
