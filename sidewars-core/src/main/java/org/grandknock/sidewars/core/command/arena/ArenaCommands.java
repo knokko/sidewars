53ca65e962362212e03396284669478fbf59b3d3
knokko <knokogator@hotmail.com>
2020-01-18 16:55:16 +0100

    Fix minor spacing problem

--- sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
+++ sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
@@ -8,6 +8,6 @@ public class ArenaCommands {
 
     @SubCommand(name="create")
     public void handleCreation(String[] args, SWCommandSender sender) {
-        sender.sendMessage(SWCommandSender.MessageType.SUCCESS, "Arena" + args[0] + " was successfully created!");
+        sender.sendMessage(SWCommandSender.MessageType.SUCCESS, "Arena " + args[0] + " was successfully created!");
     }
 }
