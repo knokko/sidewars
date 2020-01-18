db69972a6bb4a73e3f979163ed15fc275bcb468a
GrantisJ <grantasjo@gmail.com>
2020-01-18 17:41:05 +0200

    Modified ArenaCommand.java

--- sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
+++ sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
@@ -2,11 +2,13 @@ package org.grandknock.sidewars.core.command.arena;
 
 import org.grandknock.sidewars.core.command.SWCommandSender;
 import org.grandknock.sidewars.core.command.management.SubCommand;
+import org.grandknock.sidewars.core.entity.SWPlayer;
 
 public class ArenaCommands {
 
     @SubCommand(name="create")
     public void handleCreation(String[] args, SWCommandSender sender) {
-        // TODO Create arena if possible
+        SWPlayer player = (SWPlayer) sender;
+        player.sendMessage(SWCommandSender.MessageType.SUCCESS, "Arena" + args[0] + " was successfully created!");
     }
 }
