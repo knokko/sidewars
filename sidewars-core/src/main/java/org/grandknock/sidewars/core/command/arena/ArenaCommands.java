49c95dea73931d781b6c51c602cb86789a683014
knokko <knokogator@hotmail.com>
2020-01-18 16:48:49 +0100

    Also allow non-player command senders to execute /sw arena create

--- sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
+++ sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
@@ -8,7 +8,6 @@ public class ArenaCommands {
 
     @SubCommand(name="create")
     public void handleCreation(String[] args, SWCommandSender sender) {
-        SWPlayer player = (SWPlayer) sender;
-        player.sendMessage(SWCommandSender.MessageType.SUCCESS, "Arena" + args[0] + " was successfully created!");
+        sender.sendMessage(SWCommandSender.MessageType.SUCCESS, "Arena" + args[0] + " was successfully created!");
     }
 }
