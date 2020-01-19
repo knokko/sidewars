b181519818277b362f95b789e0e345fedc41c8bc
knokko <knokogator@hotmail.com>
2020-01-19 14:05:07 +0100

    Add error handling to /sw arena create

--- sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
+++ sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
@@ -1,7 +1,9 @@
 package org.grandknock.sidewars.core.command.arena;
 
 import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.world.World;
 import org.grandknock.sidewars.core.SideWars;
 import org.grandknock.sidewars.core.arena.ArenaPrototype;
 import org.grandknock.sidewars.core.command.SWCommandSender;
@@ -19,26 +21,41 @@ public class ArenaCommands {
     }
 
     @SubCommand(name="create")
-    public void handleCreation(String[] args, SWCommandSender sender) throws IncompleteRegionException {
+    public void handleCreation(String[] args, SWCommandSender sender) {
         if(sender instanceof SWPlayer) {
-            SWPlayer player = (SWPlayer) sender;
-            Region selection = player.getWorldEditSession().getSelection(player.getWorldEditSession().getSelectionWorld());
+            try {
+                SWPlayer player = (SWPlayer) sender;
+                LocalSession session = sender.getWorldEditSession();
+                World world = session.getSelectionWorld();
 
-            int minX = selection.getMinimumPoint().getX();
-            int minY = selection.getMinimumPoint().getY();
-            int minZ = selection.getMinimumPoint().getZ();
+                if (world == null)
+                    throw new IncompleteRegionException();
+                Region selection = session.getSelection(world);
 
-            int maxX = selection.getMaximumPoint().getX();
-            int maxY = selection.getMaximumPoint().getY();
-            int maxZ = selection.getMaximumPoint().getZ();
-            org.grandknock.sidewars.core.Region region = new org.grandknock.sidewars.core.Region(
-                    player.getWorldID(), minX, minY, minZ, maxX, maxY, maxZ
-            );
+                int minX = selection.getMinimumPoint().getX();
+                int minY = selection.getMinimumPoint().getY();
+                int minZ = selection.getMinimumPoint().getZ();
 
-            ArenaPrototype arenaPrototype = new ArenaPrototype(args[0], region);
-            sideWars.addArenaPrototype(arenaPrototype);
-            sender.sendMessage(MessageType.SUCCESS, "Arena " + arenaPrototype.getName() +
-                    " was created successfully");
+                int maxX = selection.getMaximumPoint().getX();
+                int maxY = selection.getMaximumPoint().getY();
+                int maxZ = selection.getMaximumPoint().getZ();
+                org.grandknock.sidewars.core.Region region = new org.grandknock.sidewars.core.Region(
+                        player.getWorldID(), minX, minY, minZ, maxX, maxY, maxZ
+                );
+
+                ArenaPrototype arenaPrototype = new ArenaPrototype(args[0], region);
+                sideWars.addArenaPrototype(arenaPrototype);
+                sender.sendMessage(MessageType.SUCCESS, "Arena " + arenaPrototype.getName() +
+                        " was created successfully");
+            } catch (IncompleteRegionException incomplete) {
+                sender.sendMessage(MessageType.ERROR,
+                        "You need a valid WorldEdit region selection to use this command.");
+            } catch (IllegalArgumentException illegal) {
+                sender.sendMessage(MessageType.ERROR, illegal.getMessage());
+            }
+        } else {
+            sender.sendMessage(MessageType.ERROR,
+                    "Only players can use this command because it requires a WorldEdit selection");
         }
     }
 
