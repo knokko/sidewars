c862bb30ead92d2a47274f13a8a970cbd48ab910
knokko <knokogator@hotmail.com>
2020-01-19 12:52:34 +0100

    Add /sw arena list and /sw arena info <name>

--- sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
+++ sidewars-core/src/main/java/org/grandknock/sidewars/core/command/arena/ArenaCommands.java
@@ -1,14 +1,14 @@
 package org.grandknock.sidewars.core.command.arena;
 
 import com.sk89q.worldedit.IncompleteRegionException;
-import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.session.SessionOwner;
 import org.grandknock.sidewars.core.SideWars;
 import org.grandknock.sidewars.core.arena.ArenaPrototype;
 import org.grandknock.sidewars.core.command.SWCommandSender;
 import org.grandknock.sidewars.core.command.management.SubCommand;
 import org.grandknock.sidewars.core.entity.SWPlayer;
+
+import static org.grandknock.sidewars.core.command.SWCommandSender.MessageType;
 
 public class ArenaCommands {
 
@@ -38,5 +38,33 @@ public class ArenaCommands {
             ArenaPrototype arenaPrototype = new ArenaPrototype(args[0], region);
             sideWars.addArenaPrototype(arenaPrototype);
         }
+    }
+
+    @SubCommand(name="list")
+    public void handleList(String[] args, SWCommandSender sender) {
+        sender.sendMessage(MessageType.INFO, "The arena's are:");
+        for (ArenaPrototype arena : sideWars.getArenaPrototypes())
+            sender.sendMessage(MessageType.INFO, arena.getName());
+    }
+
+    @SubCommand(name="info")
+    public void handleInfo(String[] args, SWCommandSender sender) {
+        if (args.length != 1) {
+            sender.sendMessage(MessageType.ERROR, "You should use /sw arena info <name>");
+            return;
+        }
+
+        ArenaPrototype arena = sideWars.getArenaPrototype(args[0]);
+        if (arena == null) {
+            sender.sendMessage(MessageType.ERROR, "There is no arena with name " + args[0]);
+            return;
+        }
+
+        sender.sendMessage(MessageType.INFO, "Info about arena " + arena.getName() + ":");
+
+        org.grandknock.sidewars.core.Region region = arena.getRegion();
+        sender.sendMessage(MessageType.INFO, "Located in world " + region.worldID +
+                " between (" + region.minX + "," + region.minY + "," + region.minZ + ") and (" +
+                region.maxX + "," + region.maxY + "," + region.maxZ + ")");
     }
 }
